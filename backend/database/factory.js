9da133931cfbc6e81a515d823f887bf61ee632bb
marlon-developer <marlonti17@gmail.com>
2020-09-02 13:26:03 -0300

    Add Factorys

--- backend/database/factory.js
+++ backend/database/factory.js
@@ -1,5 +1,7 @@
 'use strict'
 
+const Factory = require('@adonisjs/lucid/src/Factory')
+
 /*
 |--------------------------------------------------------------------------
 | Factory
@@ -12,10 +14,30 @@
 */
 
 /** @type {import('@adonisjs/lucid/src/Factory')} */
-// const Factory = use('Factory')
+const Factory = use('Factory')
 
-// Factory.blueprint('App/Models/User', (faker) => {
-//   return {
-//     username: faker.username()
-//   }
-// })
+Factory.blueprint('App/Models/User', faker => {
+  return {
+    name: faker.first(),
+    surname: faker.last(),
+    email: faker.email({ domain: 'fsocietybrasil.org' }),
+    password: 'secret'
+  }
+})
+
+Factory.blueprint('App/Models/Category', faker => {
+  return {
+    title: faker.country({ full: true }),
+    description: faker.sentence()
+  }
+})
+
+Factory.blueprint('App/Models/Product', faker => {
+  return {
+    name: faker.animal(),
+    description: faker.sentence(),
+    price: faker.floating({ min: 0, max: 1000, fixed: 2 })
+  }
+})
+
+
